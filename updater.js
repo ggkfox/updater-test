31c1c4ab6307a13a56d2160064e9045014bef33e
ggkfox <holland_74@hotmail.com>
2019-07-02 12:23:25 -0700

    before

--- updater.js
+++ updater.js
@@ -4,4 +4,11 @@ autoUpdater.logger.transports.file.level = 'info';
 
 exports.check = () => {
     autoUpdater.checkForUpdates();
+
+    autoUpdater.on('update-available', () => {
+        autoUpdater.downloadUpdate();
+    });
+    autoUpdater.on('update-downloaded', () => {
+        autoUpdater.quitAndInstall();
+    });
 };
